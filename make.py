c1a20a2e01a2de952b7f66edf6e0faf97705aada
Devin J. Cornell <devinj.cornell@gmail.com>
2021-01-12 14:00:53 -0500

    changed title link

--- make.py
+++ make.py
@@ -17,7 +17,7 @@ post_preview_template = \
 '''
 <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
     <div class="resume-content">
-        <h3 class="mb-0"><a href="/posts/<<<NAME_GOES_HERE>>>.html"><<<TITLE_GOES_HERE>>></a></h3>
+        <h3 class="mb-0"><a data-toggle="collapse" href="#blogCollapse{i}" role="button" aria-expanded="false" aria-controls="blogCollapse{i}"><<<TITLE_GOES_HERE>>></a></h3>
         <div class="subheading mb-3">By <<<AUTHOR_GOES_HERE>>> on <<<DATE_GOES_HERE>>></div>
         <<<HEADER_GOES_HERE>>>
         <p><a data-toggle="collapse" href="#blogCollapse{i}" role="button" aria-expanded="false" aria-controls="blogCollapse{i}">Expand full post...</a></p>
