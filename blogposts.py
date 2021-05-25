cf9b6f4a3c781e7b0bad3982eb1360f38073aeac
devincornell <devincornell@example.com>
2021-05-25 10:40:08 -0400

    working on python code

--- blogposts.py
+++ blogposts.py
@@ -1,61 +1,134 @@
-
+import pathlib
 import markdown
 import dateutil.parser
 
-class BlogPosts:
-    def __init__(self, post_etrees):
-
-        # create blogpost objects and sort by date
-        self.posts = [BlogPost(p) for p in post_etrees]
-        self.posts = list(sorted(self.posts, key=lambda p: p.date(), reverse=True))
-    
-    def get_html(self):
-        return '\n'.join([p.get_html() for p in self.posts])
-
-
 class BlogPost:
-    post_keys = ['post_id', 'title', 'author', 'date', 'header', 'body']
-
-    html_template = \
+    ''' Handles parsing of single blog post.
     '''
-    <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-        <div class="resume-content">
-            <h3 class="mb-0"><a data-toggle="collapse" href="#{post_id}" role="button" aria-expanded="false" aria-controls="{post_id}">{title}</a></h3>
-            <div class="subheading mb-3">{date}</div>
-            {header}
-            <p><a data-toggle="collapse" href="#{post_id}" role="button" aria-expanded="false" aria-controls="{post_id}">Expand full post...</a></p>
-            <div class="collapse" id="{post_id}">
-                {body}
-            </div>
-        </div>
-        <div class="resume-date text-md-right">
-            <span class="text-primary"></span>
-        </div>
-    </div>
+    # read in template data and read markdown file
+    def __init__(self, 
+                    post_path: pathlib.Path, # path to markdown file
+                    blogpost_template_path: pathlib.Path,
+                    blogroll_template_path: pathlib.Path
+                ):
+        self.post_path = post_path
+        self.bp_template = blogpost_template_path.read_text()
+        self.br_template = blogroll_template_path.read_text()
+
+        self.md_text = self.post_path.read_text()
+
+    ###################### Toplevel API Functions ######################
+
+    def parse_post(self):
+        ''' Parse the blog post and store components.
+        '''
+        # separate into header and body information
+        self.metadata = self.parse_metadata(self.md_text)
+        self.body = self.parse_body(self.md_text)
+
+        # convert markdown to html
+        self.post_html = markdown.markdown(self.body)
+
+    def write_post(self, outfolder_path: pathlib.Path):
+        ''' Write post to a file (call .parse_post() first).
+        '''
+        if not hasattr(self, 'post_html'):
+            raise ValueError('Must call .parse_post() before using .write_post().')
+        fpath = outfolder_path.joinpath(self.get_fname())
+
+        blogpost_text = self.format_blogpost(
+            title=self.metadata['title'],
+            subtitle=self.metadata['subtitle'],
+            date=self.metadata['date'],
+            body=self.post_html
+        )
+        fpath.write_text(blogpost_text)
+
+    ###################### Parsing Functions ######################
+
+    @classmethod
+    def parse_body(cls, markdown: str):
+        ''' Removes yaml header from top of markdown text.
+        '''
+        return '---'.join(markdown.split('---')[2:])
+
+    @classmethod
+    def parse_metadata(cls, markdown: str):
+        ''' Parses metadata at the top of the markdown text.
+        '''
+        header = markdown.split('---')[1]
+        
+        meta = dict()
+        for attr in header.split('\n'):
+            if len(attr.strip()):
+                s = attr.split(':')
+                attr, val = s[0].strip(), s[1].strip()
+                
+                # error check surrounding double quotes
+                if not (val[0] == '"' and val[-1] == '"'):
+                    raise ValueError('Blog post markdown yaml values must '
+                                        'be surrounded by double quotes.')
+                
+                # remove surrounding double quotes, save value
+                meta[attr] = val[1:-1]
+
+        return meta
+
+    ###################### String Formatting Functions ######################
+
+    def get_fname(self):
+        return f"{self.metadata['id']}.html"
+
+    def format_blogpost(self, title:str, subtitle:str, date:str, body:str):
+        ''' Return blogpost template with values substituted.
+        '''
+        return self.bp_template.format(
+            title=title,
+            subtitle=subtitle,
+            date=date,
+            body=body,
+        )
+    
+    def format_blogroll(self, path: str, title: str, subtitle:str, date:str):
+        ''' Return blogroll template with values substituted.
+        '''
+        return self.bp_template.format(
+            path=path,
+            title=title,
+            subtitle=subtitle,
+            date=date,
+        )
+
+
+class Blog:
+    ''' Main blog interface - creates BlogPosts for writing.
     '''
+    
+    def __init__(self, 
+                    markdown_folder: str, 
+                    blogpost_template_fname: str,
+                    blogroll_template_fname: str,
+                    post_folder: str
+                ):
+        self.md_path = pathlib.Path(markdown_folder)
+        self.post_path = pathlib.Path(post_folder)
+        
+        blogpost_template_path = pathlib.Path(blogpost_template_fname)
+        blogroll_template_path = pathlib.Path(blogroll_template_fname)
 
-    def __init__(self, post_etree):
-        self.data = dict()
-        for key in self.post_keys:
-            self.data[key] = self.attr(post_etree, key)
+        
+        # extract markdown files
+        self.posts = [BlogPost(p, blogpost_template_path, blogroll_template_path) 
+                        for p in self.md_path.glob('*.md')]
 
-    @staticmethod
-    def attr(post, name):
-        ''' Extract text from single attribute of a post xml element.
-        '''
-        if name in {'header', 'body'}:
-            return markdown.markdown(post.xpath(f'{name}/text()')[0])
-        else:
-            return post.xpath(f'{name}/text()')[0]
+    def parse_posts(self):
+        for post in self.posts:
+            post.parse_post()
 
-    def get_html(self):
-        ''' Get post as html string generated from template.
-        '''
-        return self.html_template.format(**self.data)
+    def write_posts(self, outfolder_fname: str):
+        outfolder_path = pathlib.Path(outfolder_fname)
+        for post in self.posts:
+            post.write_post(outfolder_path)
 
-    def date(self):
-        ''' Get parsed date (for sorting posts).
-        '''
-        return dateutil.parser.parse(self.data['date'])
 
 
--- make.py
+++ make.py
@@ -2,40 +2,19 @@
 import lxml.etree
 import glob
 
-from blogposts import BlogPosts
+from blogposts import Blog, BlogPost
 
-
-def posts_from_xml(path):
-
-    # read and parse xml from posts files
-    fnames = glob.glob(path)
-
-    post_etrees = list()
-    for fname in fnames:
-        with open(fname, 'r') as f:
-            text = f.read()
-        post_etrees += lxml.etree.fromstring(text).xpath('post')
-
-    return BlogPosts(post_etrees)
 
 if __name__ == '__main__':
-    
-    # original template file to use
-    template_fname = 'EDIT_ME_index_template.html'
-    template_replace_tag = '<BLOG_PREVIEWS_HERE/>'
 
-    # create individual post previews
-    posts = posts_from_xml('posts/*.xml')
+    # make main blog object
+    blog = Blog(
+        markdown_folder = 'post_markdown',
+        blogpost_template_fname = 'templates/blogpost_template.html',
+        blogroll_template_fname = 'templates/blogroll_template.html',
+        post_folder = 'post'
+    )
 
-    # read template file and replace values with previews
-    with open(template_fname, 'r') as f:
-        index_html = f.read().replace(template_replace_tag, posts.get_html())
-    
-    # write index file
-    with open('index.html', 'w') as f:
-        f.write(index_html)
-
-    print('wrote index.html')
 
 
 
