d28cd0c3df0487446a32cd3508a956ffd39b4586
Devin J. Cornell <devincornell@github.com>
2023-07-25 03:04:28 -0400

    adding all changes (automated commit).

--- blogmaker.py
+++ blogmaker.py
@@ -43,7 +43,10 @@ class BlogMaker:
     def render_blogroll_page(self, posts: typing.List[BlogPost]) -> str:
         '''Renders the blogroll page according to the provided template.'''
         posts = list(sorted(posts, key=lambda p: p.date, reverse=True))
-        return self.blogroll_template.render(posts=[p.as_dict() for p in posts])
+        return self.blogroll_template.render(
+            posts=[p.as_dict() for p in posts],
+            updated=datetime.datetime.now(),
+        )
 
 
 @dataclasses.dataclass
