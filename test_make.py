b645bbed542936a915dc8e03bb7ab7a62c8fb7ab
Devin J. Cornell <devincornell@github.com>
2024-11-10 22:46:24 -0500

    fixed issue with template that didn't showed rendered html

--- test_make.py
+++ test_make.py
@@ -7,21 +7,26 @@ from pprint import pprint
 import jinja2
 #from blogposts import Blog#, BlogPost
 import blogmaker
+import pymddoc
 
 
 if __name__ == '__main__':
 
     ################################# Data Science Blog #################################
 
+    if False:
+        doc = pymddoc.MarkdownDoc.from_file('post_markdown/better_dataframes_encapsulation.md')
+        print(doc.render_html())
+
     # create draft articles
     if True:
         bmaker = blogmaker.BlogMaker.read_from_markdown_files(
-            markdown_files = list(Path('draft_markdown/').glob('*.md')),
+            markdown_files = list(Path('post_markdown/').glob('*.md')),
             blogroll_template_fname = Path('templates/blogroll_template.html'),
             blogpost_template_fname = Path('templates/blogpost_template.html'),
         )
         bmaker.render_blogroll_page('test_blog.html')
 
         for post in bmaker.posts:
-            post.render_blogpost_page(target_fpath=f'draft/{post.id}.html')
+            post.render_blogpost_page(target_fpath=f'test_post/{post.id}.html')
         
